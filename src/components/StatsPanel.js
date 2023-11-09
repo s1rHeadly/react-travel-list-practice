86687be9691c7e672e7f7f7abec9be1ac3fe74a8
davidmchale <dmchale@squiz.net>
2023-11-09 13:09:28 +1100

    localstorage fix and stats panel

--- src/components/StatsPanel.js
+++ src/components/StatsPanel.js
@@ -3,11 +3,13 @@ import React from 'react'
 const StatsPanel = ({items}) => {
 
 
-  const filteredPacked = items?.filter((item) => item.packed).length;
+  const numItems = items?.length;
+  const numPacked = items?.filter((item) => item?.packed).length;
 
+ 
   return (
     <p className="stats">
-   <em>You have {items?.length || 0} items on your list, and you already packed {filteredPacked} (0%)</em>
+   <em> 💼 You have {numItems} items on your list, and you already packed {numPacked}</em>
   </p>
   )
 }
